9b977dcf68e0d9971ec0f07897c82c68bc80c2b9
MatePasto <65195236+MatePasto@users.noreply.github.com>
2020-06-01 14:08:34 -0300

    Add files via upload

--- Entregas/Mate/palette.asm
+++ Entregas/Mate/palette.asm
@@ -11,7 +11,8 @@
 	pallete1 PALETTE <01d,00d,0DEh,03d>
 	pallete2 PALETTE <78d,00d,0DDh,03d>
 .code
-INCLUDE GROUND.ASM		;Se pueden utilizar las funciones de GROUND.asm
+
+;INCLUDE GROUND.ASM		;Se pueden utilizar las funciones de GROUND.asm
 
 main proc
 	MOV AX,@DATA
@@ -19,8 +20,8 @@ main proc
 
 	nop
 
-	MOV AX,4C00h
-	INT 21h
+	mov ax, 4c00h
+	int 21h
 main endp
 
 
@@ -64,65 +65,50 @@ isPaletteAt proc
 	push cx
 	pushf
 
-iPA_QuePaleta:						;
-	cmp [di], 01d					; Pregunta que paleta esta analizando.
-	je iPA_PaletaIzquierda			;
-	cmp [di], 78d					;
-	je iPA_PaletaDerecha			;
-	jmp iPA_QuePaleta				;
+iPA_Largo:
+	xor cx, cx
+	mov cl, [di+3]
 
-iPA_PaletaIzquierda:				;
-	dec dh							;
-	pop bx							;
+iPA_Coordenadas:
 	cmp [di], dh					;
-	je iPA_NoExiste					;
-	cmp [di+1], dl					;
-	je iPA_EsquinaSuperior			; Analiza si la paleta se encuentra el la coordenada Y
-	inc dl							; ingresada y, si se encuentra, en que posicion de la
-	cmp [di+1], dl					; paleta esta.
-	je iPA_Centro					;
-	inc dl							;
-	cmp [di+1], dl					;
-	je iPA_EsquinaInferior			;
-	sub dl, 2						;
+	jne iPA_NoExiste				; Analiza las coordenadas dadas en el registro dx
+	cmp [di+1], dl					; y las compara a las del objeto para saber si la
+	je iPA_Esquina					; paleta esta en las coordenadas dadas.
+	dec dl							;
+	loop iPA_Coordenadas			;
 	jmp iPA_NoExiste				;
 
-iPA_PaletaDerecha:					;
-	inc dh							;
-	pop bx							;
-	cmp [di], dh					;
-	je iPA_NoExiste					;
-	cmp [di+1], dl					;
-	je iPA_EsquinaSuperior			; Analiza si la paleta se encuentra el la coordenada Y
-	inc dl							; ingresada y, si se encuentra, en que posicion de la
-	cmp [di+1], dl					; paleta esta.
-	je iPA_Centro					;
-	inc dl							;
-	cmp [di+1], dl					;
-	je iPA_EsquinaInferior			;
-	sub dl, 2						;
-	jmp iPA_NoExiste				;
+iPA_Esquina:
+	pop bx
+	mov ch, [di+3]
+	dec cl
+	sub ch, cl
+	cmp [di+3], ch					;
+	je iPA_EsquinaInferior			; Analiza el registro cx para saber si se
+	cmp ch, 01d						; encuentra en una esquina de la paleta o en el
+	je iPA_EsquinaSuperior			; centro de la misma.
+	jmp iPA_Centro					;
 
 iPA_EsquinaSuperior:
 	mov dl, 1
-	mov dh, 1
-	mov al, 01000000
+	mov dh, ch
+	mov al, 01000000b
 	or bl, al
 	push bx
 	jmp iPA_Fin
 
 iPA_Centro:
 	mov dl, 0
-	mov dh, 2
-	mov al, 01000000
+	mov dh, ch
+	mov al, 01000000b
 	or bl, al
 	push bx
 	jmp iPA_Fin
 
 iPA_EsquinaInferior:
 	mov dl, 1
-	mov dh, 3
-	mov al, 01000000
+	mov dh, ch
+	mov al, 01000000b
 	or bl, al
 	push bx
 	jmp iPA_Fin
@@ -130,7 +116,7 @@ iPA_EsquinaInferior:
 iPA_NoExiste:
 	mov dl, 0
 	mov dh, 0
-	mov al, 10111111
+	mov al, 10111111b
 	and bl, al
 	push bx
 	jmp iPA_Fin
